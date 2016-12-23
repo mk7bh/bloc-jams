0dc067d3aae209574017c409af08410c38790682
Brent <brenthagedorn7@gmail.com>
2016-12-23 15:01:05 -0700

    checkpoint 9 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,18 +1,31 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
  
-                 var points = document.getElementsByClassName('point');
- 
-            
+var animatePoints = function(points) {       
                  
-                var revealPoint = function (point) {
-                     point.style.opacity = 1;
-                     point.style.transform = "scaleX(1) translateY(0)";
-                     point.style.msTransform = "scaleX(1) translateY(0)";
-                     point.style.WebkitTransform = "scaleX(1) translateY(0)";
+                var revealPoint = function(index) {
+                     point[index].style.opacity = 1;
+                     point[index].style.transform = "scaleX(1) translateY(0)";
+                     point[index].style.msTransform = "scaleX(1) translateY(0)";
+                     point[index].style.WebkitTransform = "scaleX(1) translateY(0)";
                 };
                    for(var i = 0; i < points.length; i++) {
-                       revealPoint(points[i]);
+                       revealPoint(i);
                    }
                 
             };
             
+window.onload = function() {
+    
+    if(window.innerHeight > 950){
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event) {
+        if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
