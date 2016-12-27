a68bf32890980939e8929db74834bf56a5627a36
Brent <brenthagedorn7@gmail.com>
2016-12-27 11:09:57 -0700

    checkpoint 9 assignment complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,24 @@
 var pointsArray = document.getElementsByClassName('point');
- 
-var animatePoints = function(points) {       
-                 
-                var revealPoint = function(index) {
-                     point[index].style.opacity = 1;
-                     point[index].style.transform = "scaleX(1) translateY(0)";
-                     point[index].style.msTransform = "scaleX(1) translateY(0)";
-                     point[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+
+  var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
                 };
-                   for(var i = 0; i < points.length; i++) {
-                       revealPoint(i);
-                   }
-                
-            };
+var animatePoints = function(points) {       
+    forEach(points, revealPoint);             
+               
+    };
             
 window.onload = function() {
     
-    if(window.innerHeight > 950){
+    if (window.innerHeight > 950) {
         animatePoints(pointsArray);
     }
-    
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    
+       
     window.addEventListener('scroll', function(event) {
-        if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+        if(pointsArray[0].getBoundingClientRect().top <= 500) {
             animatePoints(pointsArray);
         }
     });
