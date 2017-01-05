c38aee59b617824876bf80566c3098e9107f4f4b
Brent <brenthagedorn7@gmail.com>
2017-01-05 11:37:07 -0700

    fixes

--- scripts/album.js
+++ scripts/album.js
@@ -33,7 +33,7 @@ var albumMarconi = {
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
        '<tr class="album-view-song-item">'
-     + '  <td class="song-item-number data-song-number="' + songNumber + '">' + songNumber +'</td>'
+     + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber +'</td>'
      + '  <td class="song-item-title">' + songName + '</td>'
      + '  <td class="song-item-duration">' + songLength + '</td>'
      + '</tr>'
@@ -46,7 +46,7 @@ var setCurrentAlbum = function(album) {
     var albumTitle = document.getElementsByClassName('album-view-title')[0];
     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-view-Image')[0];
+    var albumImage = document.getElementsByClassName('album-cover-art')[0];
     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
@@ -78,8 +78,8 @@ var getSongItem = function(element) {
             return element.querySelector('.song-item-number');
         case 'song-item-title':
         case 'song-item-duration':
-            retun findParentByClassName(element, 'album-view-song-item').querySelector('song-item-number');
-        case 'song-item-numnber':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
             return element;
         default:
             return;
@@ -90,19 +90,19 @@ var clickHandler = function(targetElement) {
    var songItem = getSongItem(targetElement);
     if (currentlyPlayingSong === null) {
         songItem.innerHTML = pauseButtonTemplate;
-        currentlyPlayingSong = sonItem.getAttribute('data-song-number');
+        currentlyPlayingSong = songItem.getAttribute('data-song-number');
     } else if (currentlyPlayingSong=== songItem.getAttribute('data-song-number')) {
         songItem.innerHTML = playButtonTemplate;
         currentlyPlayingSong = null;
     } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
-        var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"']');
+        var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
             currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
         songItem.innerHTML = pauseButtonTemplate;
         currentlyPlayingSong = songItem.getAttribute('data-song-number');
     }
 };
 
-var songListContatiner = document.getElementsByClassName('album-view-song-list')[0];
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
 //Album button templates
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
@@ -127,7 +127,7 @@ window.onload = function() {
     for (var i = 0; i < songRows.length; i++) {
         songRows[i].addEventListener('mouseleave', function(event) {
             var songItem = getSongItem(event.target);
-            var songItemNumber = songItem.getAttribute(data-song-number);
+            var songItemNumber = songItem.getAttribute('data-song-number');
             if (songItemNumber !== currentlyPlayingSong) {
                 songItem.innerHTML = songItemNumber;
             }
